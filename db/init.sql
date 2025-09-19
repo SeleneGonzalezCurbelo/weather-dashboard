0ecb9f733fbfe63211be699c21bdd1cb7a45e281
SeleneGonzálezCurbelo <selene.gonzalez106@alu.ulpgc.es>
2025-09-19 19:08:16 +0100

    feat(db): add columns to weather_data

--- db/init.sql
+++ db/init.sql
@@ -6,7 +6,13 @@ CREATE TABLE weather_data (
     id UUID PRIMARY KEY,
     city VARCHAR(100) NOT NULL,
     temperature FLOAT NOT NULL,
+    feels_like FLOAT,
     humidity FLOAT NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    pressure FLOAT,
+    wind_speed FLOAT,
+    wind_deg FLOAT,
+    cloudiness FLOAT,
+    description VARCHAR(255),
+    icon VARCHAR(10),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
